059796888c17934d94fb4b4a6e43e04353931267
bLuDd1 <dtavluj@gmail.com>
2023-05-30 00:59:41 +0300

    Fixed dependencies in files

--- cmd/painter/main.go
+++ cmd/painter/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"net/http"
 
-	"github.com/roman-mazur/architecture-lab-3/painter"
-	"github.com/roman-mazur/architecture-lab-3/painter/lang"
-	"github.com/roman-mazur/architecture-lab-3/ui"
+	"github.com/Snare295/architect-lab3/painter"
+	"github.com/Snare295/architect-lab3/painter/lang"
+	"github.com/Snare295/architect-lab3/ui"
 )
 
 func main() {
