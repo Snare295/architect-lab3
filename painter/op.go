9ffc0187333be77003849209c35a38198155d1e8
romalitv <youlikeyoupass@gmail.com>
2023-06-14 21:51:10 +0200

    Added operations methods

--- painter/op.go
+++ painter/op.go
@@ -1,8 +1,9 @@
 package painter
 
 import (
+	"image"
 	"image/color"
-
+	"image/draw"
 	"golang.org/x/exp/shiny/screen"
 )
 
@@ -45,4 +46,52 @@ func WhiteFill(t screen.Texture) {
 // GreenFill зафарбовує тестуру у зелений колір. Може бути викоистана як Operation через OperationFunc(GreenFill).
 func GreenFill(t screen.Texture) {
 	t.Fill(t.Bounds(), color.RGBA{G: 0xff, A: 0xff}, screen.Src)
+}
+
+type Figure struct {
+	x int
+	y int
+}
+
+func (f *Figure) Do(t screen.Texture) bool {
+	const heightHoriz int = 165
+	const widthVert int = 165
+	cYellow := color.RGBA{R: 255, G: 255, B: 0, A: 1}
+
+	t.Fill(image.Rect(f.x-widthVert/2, f.y, f.x+widthVert/2, f.y+400), cYellow, draw.Src)
+	t.Fill(image.Rect(f.x, f.y-heightHoriz/2, f.x+400, f.y+heightHoriz/2), cYellow, draw.Src)
+
+	return false
+}
+
+type Move struct {
+	x int 
+	y int
+	Figure []*Figure
+}
+
+func (m *Move) Do(t screen.Texture) bool {
+	for _, figure := range m.Figure {
+		figure.x += m.x
+		figure.y += m.y
+	}
+	return false
+}
+
+type BackRect struct {
+	x1 int
+	y1 int
+	x2 int 
+	y2 int
+}
+
+func (bcr *BackRect) Do(t screen.Texture) bool {
+	c := color.Black
+	t.Fill(image.Rect(bcr.x1, bcr.y1, bcr.x2, bcr.y2), c, screen.Src)
+	return false
+}
+
+func Reset(t screen.Texture) {
+	c := color.Black
+	t.Fill(t.Bounds(), c, draw.Src)
 }
